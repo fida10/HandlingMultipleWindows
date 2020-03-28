8a65a18aa4049768a1de11912723e7b45c99fb6b
F Eshad <fidaeshad10@gmail.com>
2020-03-28 01:13:13 -0400

    first commit

---
+++ src/HandlingMultipleWindows.java
@@ -0,0 +1,23 @@
+//this class will cycle thru all open webdriver windows, giving the handle and the URL
+import org.openqa.selenium.WebDriver;
+
+import java.util.Iterator;
+import java.util.Set;
+
+import java.util.Iterator;
+import java.util.Set;
+
+public class HandlingMultipleWindows {
+	public static void handlingMultipleWindows(WebDriver dr){
+		Set<String> windowHandles = dr.getWindowHandles();
+		Iterator<String> iWHandles = windowHandles.iterator();
+
+		for(int i = 0; i < windowHandles.size(); i++){
+			dr.switchTo().window(iWHandles.next());
+			String currentUrl = dr.getCurrentUrl();
+			String currentHandle = dr.getWindowHandle();
+			System.out.println("Current window URL is " + currentUrl + " and current handle is " + currentHandle);
+		}
+		dr.switchTo().defaultContent();
+	}
+}
